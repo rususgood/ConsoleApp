76015c6159da0e74748ebd80555e99b828803537
Hodzyk Ruslan <ruslanhodzyk2@gmail.com>
2025-11-29 14:09:56 +0100

    Fixed .gitignore

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //// Задание 1a
+        // double a = (3 + 5 * (6 + 3) - 8 * 3 + 1) / (21 + 50 / (3 + 4 * (1 + 2)));
+        // Console.WriteLine("Результат 1a = " + a);
+
+        // // Задание 1б
+        // double b = ((0.25 - 0.12) * 0.81 + 0.132 - 2.7 / 3.1) / ((3.0 / 7.0) + 2.97 * (8.05 - 8.1 * 6.07));
+        // Console.WriteLine("Результат 1б = " + b);  
+
+
+
+        //Console.WriteLine("Введите a, b и c:");
+        //int a = int.Parse(Console.ReadLine());
+        //int b = int.Parse(Console.ReadLine());
+        //int c = int.Parse(Console.ReadLine());
+
+        //double res1 = a + b - c - (3 * a * b + a * a) / (b * c) - Math.Pow(c + a * b / (double)c, 2);
+        //double res2 = (Math.Pow(a * b + 7 * c, 3) - (a * a + b * b + c * c)) / (b - a * c - a * (b + c));
+
+        //Console.WriteLine("Результат 2a = " + res1);
+        //Console.WriteLine("Результат 2б = " + res2);
+    }
+}
