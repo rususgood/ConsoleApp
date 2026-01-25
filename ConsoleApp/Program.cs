3b4f39565862fcadb5aaf609da3d0f4c1b93d0a9
Hodzyk Ruslan <ruslanhodzyk2@gmail.com>
2026-01-25 13:30:13 +0100

    new

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,14 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("А почему в ресторане ей никогда не нравится то,");
-            Console.WriteLine("что заказала она, и всегда нравится то что заказал я?");
-            Console.WriteLine();
-            Console.WriteLine("И она начинает есть у меня из тарелки. Я ей говорю:");
-            Console.WriteLine("«Закажи себе то же самое». Она говорит: «Зачем?");
-            Console.WriteLine("Я только попробовать»  И съедает половину.");
-            Console.WriteLine();
-            Console.WriteLine("// к.ф. \"О чём говорят, мужчины\" //");
+            
         }
     }
 }
