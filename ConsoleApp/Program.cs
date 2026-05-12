f52495380fb507c72289ec7b2047b37430cc43b5
Hodzyk Ruslan <ruslanhodzyk2@gmail.com>
2026-05-12 15:53:18 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введите ваше возраст:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите ваш рост в метрах:");
+        double height = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Есть ли у вас Vip-Билет?");
+        string vipInput = Console.ReadLine();
+
+        int heightInCm = (int)(height * 100);
+
+        if (age < 0 || height < 0)
         {
-            
+            Console.WriteLine("Ошибка: данные введены неверно.");
+        }
+        else
+        {
+            if ((age >= 14 && height > 150) || (vipInput == "да" && height >= 140))
+            {
+                Console.WriteLine("Проход разрешен!");
+            }
+            else
+            {
+                Console.WriteLine("Доступ запрещен.");
+            }
         }
     }
 }
