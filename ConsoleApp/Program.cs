2289d2b28a5e1764c6e696b288e85ff8bb59974c
Hodzyk Ruslan <ruslanhodzyk2@gmail.com>
2026-05-12 15:59:45 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Сумма заказа (грн): ");
+        int orderSum = int.Parse(Console.ReadLine());
+
+        Console.Write("Расстояние (км, например 5,5): ");
+        double distance = double.Parse(Console.ReadLine());
+
+        Console.Write("Есть купон на бесплатную доставку? (да/нет): ");
+        string answer = Console.ReadLine().ToLower();
+        bool hasCoupon = (answer == "да");
+
+        if (orderSum < 0 || distance < 0)
         {
-            
+            Console.WriteLine("Ошибка: данные не могут быть отрицательными.");
+        }
+        else
+        {
+            int deliveryCost = (int)(distance * 15.5);
+
+            bool isFree = (orderSum > 400 && distance < 5) || (hasCoupon && distance < 10);
+
+            if (isFree)
+            {
+                deliveryCost = 0;
+                Console.WriteLine("Доставка бесплатная!");
+            }
+            else
+            {
+                Console.WriteLine("Стоимость доставки: " + deliveryCost + " грн.");
+            }
+
+            int finalSum = orderSum + deliveryCost;
+            Console.WriteLine("Итого к оплате: " + finalSum + " грн.");
         }
     }
 }
